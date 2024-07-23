a696286d24244849028471e65bbecaa20b3f0d56
krishnapriya <krishnapriyamkkp@gmail.com>
2024-07-23 10:02:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// val=document.querySelector("#v")
+// console.log(val)
+function num(value){
+    val.value=val.value+value
+}
+
+function result(){
+    res=eval(val.value)
+    val.value=res
+}
+function cancel(){
+    val.value=""
+}
